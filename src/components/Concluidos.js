77ace1d4e7afccb845b7a9ddb9dfe6b8d8b140e9
BiancaDias <bianca.dias@ufv.br>
2023-03-06 10:39:29 -0300

    correções de layout

--- src/components/Concluidos.js
+++ src/components/Concluidos.js
@@ -4,32 +4,34 @@ import iconeQuase from "../assets/icone_quase.png"
 import parabens from "../assets/party.png"
 import putz from "../assets/sad.png"
 
-export default function Concluidos({respondidosTotais, arrayDeIconesRespondidos}){
+export default function Concluidos({ respondidosTotais, arrayDeIconesRespondidos }) {
 
-    let titulo="";
-    let mensagem="";
-    let imagem="";
-    if(arrayDeIconesRespondidos.length===8){
-        if(arrayDeIconesRespondidos.includes(iconeErrado)){
-        mensagem = "Ainda faltam alguns... Mas não desanime!"
-        titulo = "Puts..."
-        imagem = putz;
+    let titulo = "";
+    let mensagem = "";
+    let imagem = "";
+    if (arrayDeIconesRespondidos.length === 8) {
+        if (arrayDeIconesRespondidos.includes(iconeErrado)) {
+            mensagem = "Ainda faltam alguns... Mas não desanime!"
+            titulo = "Puts..."
+            imagem = putz;
         }
-        else{
+        else {
             mensagem = "Você não esqueceu de nenhum flashcard!"
             titulo = "Parabéns!"
             imagem = parabens;
         }
     }
-    return(
+    return (
         <Rodape data-test="footer">
-            <TextoFinal>
+
+            <TextoFinal >
                 <Titulo data-test="finish-text">
-                    {imagem !== "" && <img src={imagem} alt="texto final"/>}
+                    {imagem !== "" && <img src={imagem} alt="texto final" />}
                     <h2>{titulo}</h2>
                 </Titulo>
                 <p>{mensagem}</p>
             </TextoFinal>
+
             <p>{respondidosTotais}/8 CONCLUÍDOS</p>
             <div>
                 {arrayDeIconesRespondidos.map(icone => <img data-test={icone === iconeErrado ? "no-icon" : icone === iconeQuase ? "partial-icon" : "zap-icon"} src={icone} alt={icone} />)}
@@ -60,7 +62,7 @@ const Rodape = styled.footer`
     max-width: 220px;
     display: flex;
     align-items: center;
-    justify-content: space-between
+    justify-content: space-between;
     margin-top: 6px
   }
   img{
@@ -85,7 +87,7 @@ const TextoFinal = styled.div`
         margin-top: 8px;
         margin-bottom: 14px;
     }
-` 
+`
 
 const Titulo = styled.div`
     display: flex;
